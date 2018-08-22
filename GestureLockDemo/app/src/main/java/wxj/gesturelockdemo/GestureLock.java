d098f73daf24f79c0b8d5d792ec0bd6ee213a552
wangxuejiao_00 <wangxuejiao_00@163.com>
2018-08-22 17:38:09 +0800

    wxj: Add GestureLockDemo

---
+++ GestureLockDemo/app/src/main/java/wxj/gesturelockdemo/GestureLock.java
@@ -0,0 +1,28 @@
+package wxj.gesturelockdemo;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GestureLock extends View {
+
+    public GestureLock(Context context) {
+        super(context);
+        init();
+    }
+
+    public GestureLock(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public GestureLock(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init(){
+
+    }
+}
