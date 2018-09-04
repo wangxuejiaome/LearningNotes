841c4501ddaed31334eb56d27951bf5ed5179e2a
xj <wangxj@hcure.cn>
2018-09-05 01:44:25 +0800

    wxj：test protected.

---
+++ Practice/javase/src/main/java/wxj/me/javase/access/local/Foreign.java
@@ -0,0 +1,7 @@
+package wxj.me.javase.access.local;
+
+public class Foreign {
+    public static void main(String[] args) {
+        PackagedClass packagedClass = new PackagedClass();
+    }
+}
---
+++ Practice/javase/src/main/java/wxj/me/javase/access/local/PackagedClass.java
@@ -0,0 +1,7 @@
+package wxj.me.javase.access.local;
+
+class PackagedClass {
+    public PackagedClass(){
+        System.out.println("Creating a packaged class");
+    }
+}
