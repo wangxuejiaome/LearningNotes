adde1c3d9b659de20e501e68cc39cf97ba1b8550
wangxuejiao <wangxuejiaome@163.com>
2021-05-18 02:53:12 +0800

    wxj:添加底部图标

--- love_space/love_space/lib/init/index_page.dart
+++ love_space/love_space/lib/init/index_page.dart
@@ -2,24 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:love_space/home/home_page.dart';
 import 'package:love_space/learn/learn_page.dart';
 import 'package:love_space/mine/mine_page.dart';
+import 'package:love_space/util/utils.dart';
 
 class IndexPage extends StatefulWidget {
   _IndexPageState createState() => _IndexPageState();
 }
 
 class _IndexPageState extends State<IndexPage> {
-  final List<BottomNavigationBarItem> bottomTabs = [
-    BottomNavigationBarItem(icon: Icon(Icons.add_location), label: '首页'),
-    BottomNavigationBarItem(icon: Icon(Icons.book), label: '学习'),
-    BottomNavigationBarItem(icon: Icon(Icons.person), label: '我的')
-  ];
-  final List tabBodies = [HomePage(),LearnPage(),MinePage()];
+  List<BottomNavigationBarItem> bottomTabs;
+  final List tabBodies = [HomePage(), LearnPage(), LearnPage(),MinePage()];
 
   int currentIndex = 0;
   var currentPage;
 
   @override
   void initState() {
+    // 底部按钮
+    bottomTabs = [
+      BottomNavigationBarItem(
+          icon: _createBottomNavigationBarItemIcon(
+              'ic_bottom_home_normal',false),
+          activeIcon: _createBottomNavigationBarItemIcon(
+              'ic_bottom_home_active',true),
+          label: '首页'),
+      BottomNavigationBarItem(
+          icon: _createBottomNavigationBarItemIcon(
+              'ic_bottom_activity_normal',false),
+          activeIcon: _createBottomNavigationBarItemIcon(
+              'ic_bottom_activity_active',true),
+          label: '活动'),
+      BottomNavigationBarItem(
+          icon: _createBottomNavigationBarItemIcon(
+              'ic_bottom_learn_normal',false),
+          activeIcon: _createBottomNavigationBarItemIcon(
+              'ic_bottom_learn_active',true),
+          label: '学习'),
+      BottomNavigationBarItem(
+          icon: _createBottomNavigationBarItemIcon(
+              'ic_bottom_mine_normal',false),
+          activeIcon: _createBottomNavigationBarItemIcon(
+              'ic_bottom_mine_active',true),
+          label: '我的'),
+    ];
     currentPage = tabBodies[currentIndex];
     super.initState();
   }
@@ -32,7 +56,7 @@ class _IndexPageState extends State<IndexPage> {
         type: BottomNavigationBarType.fixed,
         currentIndex: currentIndex,
         items: bottomTabs,
-        onTap: (index){
+        onTap: (index) {
           setState(() {
             currentIndex = index;
             currentPage = tabBodies[currentIndex];
@@ -40,6 +64,15 @@ class _IndexPageState extends State<IndexPage> {
         },
       ),
       body: currentPage,
+    );
+  }
+
+  /// 创建底部导航栏图标
+  Image _createBottomNavigationBarItemIcon(String imageName,bool isActive) {
+    return Image(
+      image: AssetImage(Utils.getImgPath(imageName)),
+      width: isActive ? 28 : 24,
+      height: isActive ? 28: 24,
     );
   }
 }
---
+++ love_space/love_space/lib/util/utils.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Utils {
+  static String getImgPath(String name, {String format: 'png'}) {
+    return 'assets/images/$name.$format';
+  }
+}
+
+/// 默认dialog背景色为半透明黑色，这里修改源码改为透明
+// Future<T> showTransparentDialog<T>({
+//   @required BuildContext context,
+//   bool barrierDismissible = true,
+//   WidgetBuilder builder,
+// }) {
+//
+//   final ThemeData theme = Theme.of(context, shadowThemeOnly: true);
+//   return showGeneralDialog(
+//     context: context,
+//     pageBuilder: (BuildContext buildContext, Animation<double> animation, Animation<double> secondaryAnimation) {
+//       final Widget pageChild = Builder(builder: builder);
+//       return SafeArea(
+//         child: Builder(
+//             builder: (BuildContext context) {
+//               return theme != null
+//                   ? Theme(data: theme, child: pageChild)
+//                   : pageChild;
+//             }
+//         ),
+//       );
+//     },
+//     barrierDismissible: barrierDismissible,
+//     barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+//     barrierColor: Color(0x00FFFFFF),
+//     transitionDuration: const Duration(milliseconds: 150),
+//     transitionBuilder: _buildMaterialDialogTransitions,
+//   );
+// }
+//
+// Widget _buildMaterialDialogTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+//   return FadeTransition(
+//     opacity: CurvedAnimation(
+//       parent: animation,
+//       curve: Curves.easeOut,
+//     ),
+//     child: child,
+//   );
+// }
+
