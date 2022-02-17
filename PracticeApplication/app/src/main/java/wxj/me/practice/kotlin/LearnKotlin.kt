bf90cd5536dbb40240e379c2a8317a856798da23
wangxuejiao <wangxuejiaome@163.com>
2022-02-17 11:00:45 +0800

    wxj:添加 kotlin 模块

---
+++ PracticeApplication/app/src/main/java/wxj/me/practice/kotlin/LearnKotlin.kt
@@ -0,0 +1,13 @@
+package wxj.me.practice.kotlin
+
+/**
+ *    author : wangxuejiao@leoao.com
+ *    date   : 2022/2/17
+ *    desc   :
+ */
+class LearnKotlin {
+
+    fun main() {
+        println("Hello Kotlin");
+    }
+}
